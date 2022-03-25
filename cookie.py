90b3141b3d222131de5d1bef214569417f67057a
海不眠 <64032294+catiz@users.noreply.github.com>
2022-03-25 22:13:43 +0800

    Update cookie.py

--- cookie.py
+++ cookie.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     driver.switch_to_frame("login_frame")
     time.sleep(1)
     driver.find_element_by_xpath("/html/body/div[1]/div[9]/a[1]").click()
-    driver.find_element_by_xpath("/html/body/div[1]/div[5]/div/div[1]/div[3]/form/div[1]/div/input").send_keys("2904306586") # QQ号
+    driver.find_element_by_xpath("/html/body/div[1]/div[5]/div/div[1]/div[3]/form/div[1]/div/input").send_keys("10001") # QQ号
     driver.find_element_by_xpath("/html/body/div[1]/div[5]/div/div[1]/div[3]/form/div[2]/div[1]/input").send_keys("********") # QQ密码
 
     time.sleep(1)
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     driver.save_screenshot('/www/wwwroot/image/image/'+str(times)+'.png')
     requests.post(
         url="https://oapi.dingtalk.com/robot/send?access_token=444c407643bc23a82697d813a2fd4965e773ff617c89352fb17b5ff4fdc85d2c",
-        headers=headers, data=json.dumps({"markdown": {"title":"登录QQ","text": "#### 登录QQ ####\n![screenshot](http://81.69.97.199/image/"+str(times)+".png)\n"}, "msgtype": "markdown"}))
+        headers=headers, data=json.dumps({"markdown": {"title":"登录QQ","text": "#### 登录QQ ####\n![screenshot](http://999.999.999.999/image/"+str(times)+".png)\n"}, "msgtype": "markdown"}))
     time.sleep(30)
     # 通常情况下在服务器会需要扫码登录，讲输入密码登录后的截图发送到钉钉查看，如果需要扫码就扫码登录，如果已经登录成功，30秒后即可保存cookie
     
